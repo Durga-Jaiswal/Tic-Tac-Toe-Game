42da3d2ae3a34a21b94d7cf4c282db7a257bfc93
Durga Jaiswal <jaiswaldurga555@gmail.com>
2023-12-12 16:09:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import Board from './components/Board.jsx'
+import './App.css'
+function App() {
+  
+  
+  return (
+    <>
+      <Board />
+    </>
+  )
+}
+
+export default App;
